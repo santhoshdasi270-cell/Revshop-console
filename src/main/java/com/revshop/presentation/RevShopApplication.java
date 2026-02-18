258111984a9094313160a03a9300a3d2bb0f5172
Santhosh Dasi <santhoshdasi270@gmail.com>
2026-02-18 17:35:15 +0530

    Added full project structure

---
+++ src/main/java/com/revshop/presentation/RevShopApplication.java
@@ -0,0 +1,12 @@
+package com.revshop.presentation;
+
+public class RevShopApplication {
+
+    public static void main(String[] args) {
+        System.out.println("===================================");
+        System.out.println("      Welcome to RevShop");
+        System.out.println(" Secure Console E-Commerce App ");
+        System.out.println("===================================");
+    }
+}
+
